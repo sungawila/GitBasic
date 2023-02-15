3199ca80365fb72f7360c181e96cc4c016e260f9
Ibrahim Mafaz <mafaz.uop@gmail.com>
2023-02-15 17:36:17 +0530

    Second commit

--- Git/src/git/GitTest.java
+++ Git/src/git/GitTest.java
@@ -6,7 +6,11 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World");
-
+		//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
 	}
 
+	public void methid1() {
+		
+		System.out.println("Changes");
+	}
 }
